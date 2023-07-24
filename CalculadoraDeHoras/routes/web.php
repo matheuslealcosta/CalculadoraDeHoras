edb97bcb33475c8e1d2855eb438d58e51aa7f33d
MatGod135 <matheuslealcosta@icloud.com>
2023-07-24 13:28:52 -0300

    define rotas e views

--- CalculadoraDeHoras/routes/web.php
+++ CalculadoraDeHoras/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/horas', function (){
+    return view('horas');
 });
+
+Route::post('/horas', [HorasController::class, 'calculaHoras'])->name('calcular.horas');
