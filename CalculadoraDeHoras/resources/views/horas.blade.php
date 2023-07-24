0d86f0e0f8a1605e38421c10d04a1c1ff8018029
MatGod135 <matheuslealcosta@icloud.com>
2023-07-24 14:20:51 -0300

    adiciona estilização dentro da view por problemas de diretorio

--- CalculadoraDeHoras/resources/views/horas.blade.php
+++ CalculadoraDeHoras/resources/views/horas.blade.php
@@ -5,9 +5,74 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de horas</title>
+    <style>
+        body {
+            background-color: #f0f0f0;
+            color: #333;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            margin-top: 30px;
+        }
+
+        form {
+            margin-top: 20px;
+        }
+
+        label {
+            font-size: 20px;
+            margin-right: 5px;
+        }
+
+        input[type="time"] {
+            font-size: 18px;
+            padding: 5px;
+        }
+
+        #calcularBtn {
+            font-size: 18px;
+            padding: 8px 15px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+            margin-top: 10px;
+        }
+
+        #resultado {
+            margin-top: 30px;
+            font-size: 20px;
+        }
+
+        #resultado p {
+            margin-bottom: 10px;
+        }
+
+        #resultado button {
+            font-size: 18px;
+            padding: 8px 15px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+        }
+
+        .night-theme {
+            background-color: #333;
+            color: #fff;
+        }
+
+        .night-theme button {
+            background-color: #3a3a3a;
+        }
+    </style>
 </head>
-<body class="matheus">
-    <h1 class="matheus">Calculadora de Horas 🕒</h1>
+<body>
+    <h1>Calculadora de Horas 🕒</h1>
     <form id="calcularForm">
         @csrf
         <label for="inicio">Hora de Início:</label>
@@ -45,8 +110,6 @@
                     <p>Horas Noturnas: ${data.horas_noturnas}</p>
                     <button onclick="goBack()">Voltar</button>
                 `;
-
-                // Apply night theme
                 document.body.classList.add('night-theme');
             })
             .catch(error => {
